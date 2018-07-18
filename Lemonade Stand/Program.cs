dd209885a5534727a5cd58f05273edeeb6ac3015
Shir <healthylegacy01@gmail.com>
2018-07-18 09:47:32 -0500

    Add project files.

---
+++ Lemonade Stand/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lemonade_Stand
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
