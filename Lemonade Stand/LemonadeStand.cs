a7d21c870262d81dfa05f55564efb7a5c4e57c34
Chip <chip.elmer@gmail.com>
2018-08-17 10:06:11 -0500

    Added LemonadeStand class and refactored to use it

---
+++ Lemonade Stand/LemonadeStand.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lemonade_Stand
+{
+    class LemonadeStand
+    {
+        public string Name { get; set; }
+        public int NumberOfCups { get; set; }
+        public int PricePerCup { get; set; }
+        public int CostPerCup { get; set; }
+
+        public int TotalRevenue()
+        {
+            return NumberOfCups * PricePerCup;
+        }
+
+        public int TotalExpenses()
+        {
+            return NumberOfCups * PricePerCup;
+        }
+
+        public int TotalProfit()
+        {
+            return TotalRevenue() - TotalExpenses();
+        }
+    }
+}
--- Lemonade Stand/Program.cs
+++ Lemonade Stand/Program.cs
@@ -6,45 +6,40 @@ namespace Lemonade_Stand
     {
         static void Main(string[] args)
         {
+            LemonadeStand stand = new LemonadeStand();
+
             Console.WriteLine("Is This a Good idea?");
             Console.WriteLine();
             Console.WriteLine("What would you like your lemonade to be called?");
-            String LemonadeStandName = Console.ReadLine();
+            stand.Name = Console.ReadLine();
 
-            Console.WriteLine("Thank You, Your lemonade stand will be called " + LemonadeStandName);
+            Console.WriteLine("Thank You, Your lemonade stand will be called " + stand.Name);
             Console.WriteLine();
             Console.WriteLine("How many cups do you plan to sell?");
 
-            int NumberOfCups = Int32.Parse(Console.ReadLine());
+            stand.NumberOfCups = Int32.Parse(Console.ReadLine());
 
             Console.WriteLine("Thank you");
             Console.WriteLine();
             Console.WriteLine("How much will you sell each cup for?");
 
-            int PricePerCup = Int32.Parse(Console.ReadLine());
+            stand.PricePerCup = Int32.Parse(Console.ReadLine());
 
-            int TotalRevenue = NumberOfCups * PricePerCup;
-
-            Console.WriteLine("Your total revenue will be $ " + TotalRevenue);
+            Console.WriteLine("Your total revenue will be $ " + stand.TotalRevenue());
             Console.WriteLine();
 
             Console.WriteLine("How much will your cost per cup be?");
 
-            int CostPerCups = Int32.Parse(Console.ReadLine());
+            stand.CostPerCup = Int32.Parse(Console.ReadLine());
             Console.WriteLine();
-            int CostOfCups = NumberOfCups * CostPerCups;
-            int TotalProfit = TotalRevenue - CostOfCups;
-            Console.WriteLine("Your Lemonade stand, " + LemonadeStandName + ", will have a Total Revenue of $" + TotalRevenue + " at a cost of $ " + CostOfCups);
-            Console.WriteLine("Your profit will be $" + TotalProfit);
+            
+            Console.WriteLine("Your Lemonade stand, " + stand.Name + ", will have a Total Revenue of $" + stand.TotalRevenue() + " at a cost of $ " + stand.TotalExpenses());
+            Console.WriteLine("Your profit will be $" + stand.TotalProfit());
             Console.WriteLine("Does this sound good or bad?");
             Console.WriteLine();
 
             Console.WriteLine("Would you like to proceed?");
             string UserResponse = Console.ReadLine();
-
-
-
-
 
             Console.ReadLine();
         }
